e89b17e09340c99482f11f17b18caf1e68d283c6
questarc <questarc.tech@gmail.com>
2025-12-26 13:41:47 -0500

    Add get_explanations function for statistical terms

---
+++ utils/explanations.py
@@ -0,0 +1,9 @@
+def get_explanations():
+    return {
+        'Mean': 'The average of a set of numbers.',
+        'Median': 'The middle value when numbers are sorted.',
+        'Mode': 'The most frequently occurring number.',
+        'Variance': 'How spread out the numbers are.',
+        'Standard Deviation': 'The average amount numbers differ from the mean.',
+        'Probability': 'The chance of an event happening.'
+    }
